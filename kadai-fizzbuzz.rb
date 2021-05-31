c4e2d95480ad788b356f3b66f003828a2a1a0f17
koki.naito <ssstututusss617@gmail.com>
2021-05-31 06:03:07 +0000

    update kadai-fizzbuzz.rb

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -2,13 +2,13 @@ num_max = 100
 
 def fizzbuzz(num)
     if num % 3 == 0 && num % 5 == 0 
-            puts "FizzBuzz"
+      "FizzBuzz"
     elsif num % 3 == 0
-            puts "Fizz"
+      "Fizz"
     elsif num % 5 == 0
-            puts "Buzz"
+      "Buzz"
     else
-            puts num
+      num
     end
 end
 
